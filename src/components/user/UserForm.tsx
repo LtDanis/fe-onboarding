cf2cb956784bd4825248a6cabeeef28f31117e4a
Danielius <danielius55@yahoo.com>
2024-08-02 11:30:59 +0300

    remove user comments to separate tab

--- src/components/user/UserForm.tsx
+++ src/components/user/UserForm.tsx
@@ -17,6 +17,8 @@ export default function UserForm({
   const [userName, setUserName] = useState(user?.name || "")
   const [userSurname, setUserSurname] = useState(user?.surname || "")
 
+  const [generalInfoSelected, setGeneralInfoSelected] = useState(true)
+
   const [pageState, setPageState] = useState(PAGE_STATE.loading)
   const [departments, setDepartments] = useState<Department[]>([])
   const [positions, setPositions] = useState<Position[]>([])
@@ -44,93 +46,108 @@ export default function UserForm({
     setUserSurname(user?.surname || "")
   }, [user])
 
+  const selectGeneralInformation = () => setGeneralInfoSelected(true)
+  const selectComments = () => setGeneralInfoSelected(false)
+
   return (
     <>
       {pageState === PAGE_STATE.completed ? (
         <form className="form-input" onSubmit={onSubmitForm}>
-          <div className="p-5 form-header">General information</div>
-          <div className="p-5 form-header">Comments</div>
-
-          <div className="p-5">
-            <input
-              className="w-full"
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              required
-              autoComplete="off"
-            />
+          <div className="p-5 form-header">
+            <button type="button" onClick={selectGeneralInformation}>
+              General information
+            </button>
+          </div>
+          <div className="p-5 form-header">
+            <button type="button" onClick={selectComments}>
+              Comments
+            </button>
           </div>
 
-          <div className="p-5">
-            <input
-              className="w-full"
-              type="text"
-              id="surname"
-              name="surname"
-              placeholder="Surname"
-              value={userSurname}
-              onChange={(e) => setUserSurname(e.target.value)}
-              required
-              autoComplete="off"
-            />
-          </div>
+          {generalInfoSelected ? (
+            <>
+              <div className="p-5">
+                <input
+                  className="w-full"
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  required
+                  autoComplete="off"
+                />
+              </div>
 
-          <div className="p-5">
-            <select
-              className="w-full"
-              id="position"
-              name="position"
-              required
-              autoComplete="off"
-            >
-              {positions.map((position: Position) => (
-                <option value={position.id}>{position.name}</option>
-              ))}
-            </select>
-          </div>
+              <div className="p-5">
+                <input
+                  className="w-full"
+                  type="text"
+                  id="surname"
+                  name="surname"
+                  placeholder="Surname"
+                  value={userSurname}
+                  onChange={(e) => setUserSurname(e.target.value)}
+                  required
+                  autoComplete="off"
+                />
+              </div>
 
-          <div className="p-5">
-            <select
-              className="w-full"
-              id="department"
-              name="department"
-              required
-              autoComplete="off"
-            >
-              {departments.map((department: Department) => (
-                <option value={department.id}>{department.name}</option>
-              ))}
-            </select>
-          </div>
+              <div className="p-5">
+                <select
+                  className="w-full"
+                  id="position"
+                  name="position"
+                  required
+                  autoComplete="off"
+                >
+                  {positions.map((position: Position) => (
+                    <option value={position.id}>{position.name}</option>
+                  ))}
+                </select>
+              </div>
 
-          <div className="p-5">
-            <label htmlFor="image">Image</label>
-            <input
-              className="w-full"
-              type="file"
-              accept="image/*"
-              id="image"
-              name="image"
-              placeholder="Image"
-              autoComplete="off"
-            />
-            <Avatar image={user?.image} />
-          </div>
+              <div className="p-5">
+                <select
+                  className="w-full"
+                  id="department"
+                  name="department"
+                  required
+                  autoComplete="off"
+                >
+                  {departments.map((department: Department) => (
+                    <option value={department.id}>{department.name}</option>
+                  ))}
+                </select>
+              </div>
 
-          <div className="p-5">
-            <input
-              className="w-full"
-              type="text"
-              id="comment"
-              name="comment"
-              placeholder="Comments"
-              autoComplete="off"
-            />
-          </div>
+              <div className="p-5">
+                <label htmlFor="image">Image</label>
+                <input
+                  className="w-full"
+                  type="file"
+                  accept="image/*"
+                  id="image"
+                  name="image"
+                  placeholder="Image"
+                  autoComplete="off"
+                />
+                <Avatar image={user?.image} />
+              </div>
+            </>
+          ) : (
+            <div className="p-5">
+              <input
+                className="w-full"
+                type="text"
+                id="comment"
+                name="comment"
+                placeholder="Comments"
+                autoComplete="off"
+              />
+            </div>
+          )}
 
           <div className="wrap">
             <button type="submit" className="button">
